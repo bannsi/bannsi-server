a5c7eca064b17aa08bcf504b3d707e3064127d9e
jinho <hahoh0013@gmail.com>
2021-11-12 13:52:16 +0900

    peice service

---
+++ peice-service/src/main/java/com/bannsi/peiceservice/PeiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bannsi.peiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeiceServiceApplication.class, args);
+	}
+
+}
