55ac872abccb754548128da1875353fcac147166
jinho <hahoh0013@gmail.com>
2021-11-12 13:10:41 +0900

    config server

---
+++ confsvr/src/test/java/com/bannsi/confsvr/ConfsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bannsi.confsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfsvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
