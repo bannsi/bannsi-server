05bd29380811f84037cb7693ce342fda71059e04
zzzinho <hahoh0013@gmail.com>
2021-11-20 12:48:14 +0900

    peice

---
+++ peice-service/src/main/java/com/bannsi/peiceservice/model/PeiceImage.java
@@ -0,0 +1,21 @@
+package com.bannsi.peiceservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name="peice_images")
+public class PeiceImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="image_id", nullable = false)
+    private Long imageId;
+        
+}
