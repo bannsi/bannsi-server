1ccb55269b888e4d91ced4f10d9401792aa9ad54
zzzinho <hahoh0013@gmail.com>
2021-11-25 15:07:15 +0900

    peice

--- gateway/src/main/java/com/bannsi/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/bannsi/gateway/GatewayApplication.java
@@ -1,7 +1,15 @@
 package com.bannsi.gateway;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +18,17 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Autowired
+	RouteDefinitionLocator locator;
+
+	@Bean
+	public List<GroupedOpenApi> apis() {
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*service")).forEach(routeDefinition -> {
+			String name = routeDefinition.getId().replaceAll("service", "s");
+			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").setGroup(name).build());
+		});
+		return groups;
+	}
 }
